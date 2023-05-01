f07fe7c0d723004dd50b078855bad80037c64c57
pxy <benlaixy@163.com>
2023-05-01 22:23:39 +0800

    advanced

--- src/test/java/com/xxx/netty/d1/EventLoopServer.java
+++ src/test/java/com/xxx/netty/d1/EventLoopServer.java
@@ -5,6 +5,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -18,14 +19,24 @@ import java.nio.charset.Charset;
 @Slf4j
 public class EventLoopServer {
     public static void main(String[] args) {
+        //改进2：创建一个独立的EventLoopGroup  处理耗时较长的handler
+        DefaultEventLoopGroup group = new DefaultEventLoopGroup();
         new ServerBootstrap()
-                //建立主次关系，任务分明  boss负责accept worker负责rw
+                //改进1：建立主次关系，任务分明  boss负责accept worker负责rw
                 .group(new NioEventLoopGroup(),new NioEventLoopGroup(2))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+
+                        ch.pipeline().addLast("h1",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(buf);
+                            }
+                        }).addLast(group,"h2",new ChannelInboundHandlerAdapter(){
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
                                 ByteBuf buf = (ByteBuf) msg;
